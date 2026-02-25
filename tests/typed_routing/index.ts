a6d17d8c2736f73864aeae3f2c0a408751cf6d7b
axtk <axtkx@inbox.ru>
2026-02-25 16:59:24 +0300

    add typed routing test

---
+++ tests/typed_routing/index.ts
@@ -0,0 +1,46 @@
+import { createURLSchema } from "url-shape";
+import { z } from "zod";
+import { Route } from "../../src/Route.ts";
+import "./index.css";
+
+let { url } = createURLSchema({
+  "/": z.object({}),
+  "/sections/:id": z.object({
+    params: z.object({
+      id: z.coerce.number(),
+    }),
+  }),
+});
+
+let route = new Route();
+
+route.on("navigationcomplete", () => {
+  renderHeader();
+  renderMainContent();
+});
+
+route.observe(document);
+
+function renderHeader() {
+  document.querySelector("header")!.className =
+    route.href === url("/").href ? "full" : "compact";
+}
+
+function renderMainContent() {
+  let { ok: isSection, params } = route.match(url("/sections/:id"));
+
+  if (isSection)
+    document.querySelector('[data-id="section"] h2 span')!.textContent = String(
+      params.id,
+    );
+
+  document
+    .querySelector('main[data-id="intro"]')!
+    .toggleAttribute("hidden", isSection);
+
+  document
+    .querySelector('main[data-id="section"]')!
+    .toggleAttribute("hidden", !isSection);
+
+  document.body.removeAttribute("hidden");
+}
